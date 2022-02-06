959ac14b0dba53abdeefb4eb5309aa02b7929baf
Prou Yann <toofff.development@gmail.com>
2022-02-06 00:24:00 -0500

    feat: move DragulaJS in src folder

--- test/classes.js
+++ test/classes.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('tape');
-var classes = require('../classes');
+var classes = require('../src/classes');
 
 test('classes exports the expected api', function (t) {
   t.equal(typeof classes.add, 'function', 'classes.add is a method');
